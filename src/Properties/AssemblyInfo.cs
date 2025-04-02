ef3cc9ae269cbd9e4d14505ee3f865e2120f0a4f
xavier2910 <x.distelzweig@gmail.com>
2025-04-02 18:54:33 -0500

    added KSPAssembly* attributes and a dependency on MM (for later)

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MusicSwitcher")]
+[assembly: AssemblyDescription("Affero GPL v3.0 license")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MusicSwitcher")]
+[assembly: AssemblyCopyright("Xavier Distelzweig")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("neutral")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// If you do set it, you'll need to set a GUID too
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
+[assembly: KSPAssembly("MusicSwitcher", 0, 0, 0)]
+// for whatever reason, MM 4.2.3 has KSPAssembly(2,5)
+[assembly: KSPAssemblyDependency("ModuleManager", 2, 5)]
