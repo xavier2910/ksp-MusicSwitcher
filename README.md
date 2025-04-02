f8fb52c64c7ba68a331f46d3ef196a335101f13d
xavier2910 <x.distelzweig@gmail.com>
2025-04-02 08:13:06 -0500

    created project files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ksp-MusicSwitcher
-Plugin to change the music in Kerbal Space Program
+Plugin to change the music in Kerbal Space Program.
+Compatible with versions 1.12.3-1.12.5.
+
+## Dependencies
+* Mutiny
---
+++ src/MusicSwitcher.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MusicSwitcher
+{    
+    //[RequireComponent(typeof(AudioSource))]
+    public class MusicSwitcher : MonoBehaviour {
+        
+    }
+}
