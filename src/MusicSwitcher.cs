ad8a716530521627c07dd3a95ec0e51159cbc117
xavier2910 <x.distelzweig@gmail.com>
2025-04-02 09:40:55 -0500

    successfully hijacked MusicLogic

--- src/MusicSwitcher.cs
+++ src/MusicSwitcher.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 namespace MusicSwitcher
 {    
-    //[RequireComponent(typeof(AudioSource))]
     public class MusicSwitcher : MonoBehaviour {
-        
+
+        private AudioSource layer1;
+        private AudioSource layer2;
+        private AudioSource layer3;
+
+        private MusicLogic tameMusicLogic;
+
+        private void Awake() {
+            tameMusicLogic = GetComponent<MusicLogic>();
+            tameMusicLogic.enabled = false;
+
+            var asrcs = GetComponents<AudioSource>();
+            foreach (var asrc in asrcs)
+            {
+                asrc.enabled = false;
+            }
+        }
+
+        private void Start() {
+            layer1 = gameObject.AddComponent<AudioSource>();
+            layer2 = gameObject.AddComponent<AudioSource>();
+            layer3 = gameObject.AddComponent<AudioSource>();
+
+            tag = Statics.kMusicSwitcherTag;
+        }
     }
 }
