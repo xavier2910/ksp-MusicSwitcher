d9b50ef84d4da5cae340e6ccb32ed7a46996e4b7
xavier2910 <x.distelzweig@gmail.com>
2025-04-03 11:50:01 -0500

    converted to sdk-style! all the things work!!

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MusicSwitcher")]
-[assembly: AssemblyDescription("Affero GPL v3.0 license")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MusicSwitcher")]
-[assembly: AssemblyCopyright("Xavier Distelzweig")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("neutral")]
+// [assembly: AssemblyTitle("MusicSwitcher")]
+// [assembly: AssemblyDescription("Affero GPL v3.0 license")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("")]
+// [assembly: AssemblyProduct("MusicSwitcher")]
+// [assembly: AssemblyCopyright("Xavier Distelzweig")]
+// [assembly: AssemblyTrademark("")]
+// [assembly: AssemblyCulture("neutral")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,8 +20,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 // If you do set it, you'll need to set a GUID too
 
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+// [assembly: AssemblyVersion("0.0.0.0")]
+// [assembly: AssemblyFileVersion("0.0.0.0")]
 
 [assembly: KSPAssembly("MusicSwitcher", 0, 0, 0)]
 // for whatever reason, MM 4.2.3 has KSPAssembly(2,5)
