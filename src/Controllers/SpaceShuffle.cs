0da1d6a9714f7eec6ce91f999f710c6a6c18c14d
xavier2910 <x.distelzweig@gmail.com>
2025-05-15 18:33:54 -0500

    upgraded SpaceShuffle to use the more robust FSM technique used by SituationalShuffle

--- src/Controllers/SpaceShuffle.cs
+++ src/Controllers/SpaceShuffle.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using MusicSwitcher.Util;
 using UnityEngine;
 using Log = KSPBuildTools.Log;
 
@@ -9,18 +10,17 @@ namespace MusicSwitcher.Controllers {
 
         private AudioSourceWrangler w;
         private AudioSource src;
+        private readonly CoroutineManager coroutines;
 
         private readonly List<AudioClip> tracks;
         private int currentTrack = int.MaxValue; // set to max int to induce a shuffle immediately on play start.
+        private State currentState = State.INACTIVE;
         private bool InSpace { get => FlightGlobals.ActiveVessel.orbit.referenceBody.bodyName != "Kerbin"
                                    || FlightGlobals.ActiveVessel.situation == Vessel.Situations.ORBITING
                                    || FlightGlobals.ActiveVessel.situation == Vessel.Situations.SUB_ORBITAL
                                    || FlightGlobals.ActiveVessel.situation == Vessel.Situations.ESCAPING
                                    || FlightGlobals.ActiveVessel.situation == Vessel.Situations.DOCKED;
         }
-        private bool paused = true;
-
-        private bool closed = false;
 
         private readonly System.Random rnd;
         private readonly string logTag = "[SpaceShuffle]";
@@ -29,8 +29,10 @@ namespace MusicSwitcher.Controllers {
             rnd = new System.Random();
             tracks = new List<AudioClip>();
             _ = logTag;
+            coroutines = new CoroutineManager();
         }
 
+        #region IController
         public void Initialize(AudioSourceWrangler w, ConfigNode node) {
             this.w = w;
             src = w.Get();
@@ -44,23 +46,17 @@ namespace MusicSwitcher.Controllers {
                 Log.Warning($"Controller '{cfg.debugName}' has no associated tracks!", logTag);
             }
 
+            if (InSpace) {
+                currentState = State.ACTIVE;
+            } else {
+                currentState = State.INACTIVE;
+            }
         }
 
         public void Add(AudioClip c) => tracks.Add(c);
 
-        public void Pause() {
-            src.Pause();
-            paused = true;
-        }
-        public void UnPause() {
-            if (InSpace) {
-                src.UnPause();
-                paused = false;
-            }
-        }
-
         public void Close() {
-            if (closed) {
+            if (currentState == State.CLOSED) {
                 return;
             }
 
@@ -68,38 +64,102 @@ namespace MusicSwitcher.Controllers {
             src.Stop();
             src = null;
             tracks.Clear();
-            closed = true;
+            currentState = State.CLOSED;
             ReleaseEvents();
         }
 
         public void Update() {
-            if (closed) {
-                return;
-            }
-            if (tracks.Count == 0) {
-                return;
-            }
-            if (!paused && !InSpace) {
-                Pause();
-            }
-            if (paused) {
-                return;
-            }
-            if (StillPlaying()) {
-                return;
-            }
+            UpdateState();
+            DispatchState();
 
-            if (currentTrack > tracks.Count) {
-                currentTrack = 0;
-                ShuffleTracks();
-            }
-            src.clip = tracks[currentTrack];
-            src.Play();
-            currentTrack++;
+            UpdateCoroutines();
         }
 
-        private bool StillPlaying() {
-            return src.isPlaying;
+        #endregion
+        #region StateMachine
+
+        private enum State {
+            INACTIVE,            // ie not currently in the correct state and so not playing music
+            ACTIVE,              // playing music
+            PAUSED_WHILE_ACTIVE, // game paused
+            PAUSING,
+            UNPAUSING,
+            CLOSED,              // this IController is no longer being used and should noop
+                                 // until the garbage collector comes for it
+
+            /* State Diagram:
+                                                            PAUSING
+                                                        +--game paused--> PAUSED_WHILE_ACTIVE  \
+                                                        |                      |                \
+                 +--------Ship enters space--------> ACTIVE <---game unpaused--+                 }--Close()  called--> CLOSED
+                 |                                     |         UNPAUSING                      /   or tracks empty
+             INACTIVE <-------Ship leaves space--------+                                       /
+
+                Note that we don't need to worry about pausing while CLOSED or INACTIVE, since those states
+                produce no sound anyway.
+            */
+        }
+
+        private void UpdateState() {
+            switch (currentState) {
+                case State.CLOSED:
+                    break;
+                case State.PAUSING:
+                    break;
+                case State.UNPAUSING:
+                    break;
+                case State.PAUSED_WHILE_ACTIVE:
+                    break;
+                case State.ACTIVE:
+                    if (!InSpace) {
+                        Deactivate();
+                    }
+                    break;
+                case State.INACTIVE:
+                    if (InSpace) {
+                        Activate();
+                    }
+                    break;
+            }
+        }
+
+        private void Activate() {
+            currentState = State.ACTIVE;
+            currentTrack = int.MaxValue;
+        }
+
+        private void Deactivate() {
+            currentState = State.INACTIVE;
+            src.Stop();
+        }
+
+        private void DispatchState() {
+            switch (currentState) {
+                case State.CLOSED:
+                    break;
+                case State.PAUSED_WHILE_ACTIVE:
+                    break;
+                case State.PAUSING:
+                    break;
+                case State.UNPAUSING:
+                    break;
+                case State.ACTIVE:
+                    if (ReadyForNextTrack()) {
+                        if (currentTrack >= tracks.Count) {
+                            ShuffleTracks();
+                            currentTrack = 0;
+                        }
+                        src.clip = tracks[currentTrack++];
+                        src.Play();
+                    }
+                    break;
+                case State.INACTIVE:
+                    break;
+            }
+        }
+
+        private bool ReadyForNextTrack() {
+            return !src.isPlaying && currentState == State.ACTIVE;
         }
 
         private void ShuffleTracks() {
@@ -112,26 +172,36 @@ namespace MusicSwitcher.Controllers {
             }
         }
 
+        public void Pause() {
+            if (currentState == State.ACTIVE) {
+                src.Pause();
+                currentState = State.PAUSED_WHILE_ACTIVE;
+            }
+        }
+        public void UnPause() {
+            if (currentState == State.PAUSED_WHILE_ACTIVE) {
+                src.UnPause();
+                currentState = State.ACTIVE;
+            }
+        }
+
+        #endregion
+        #region Coroutines
+
+        private void UpdateCoroutines() {}
+
+        #endregion
         #region Events
 
         private void BindEvents() {
-            GameEvents.onGamePause.Add(OnGamePause);
-            GameEvents.onGameUnpause.Add(OnGameUnPause);
-            GameEvents.VesselSituation.onReachSpace.Add(OnEnterSpace);
+            GameEvents.onGamePause.Add(Pause);
+            GameEvents.onGameUnpause.Add(UnPause);
         }
 
         private void ReleaseEvents() {
-            GameEvents.onGamePause.Remove(OnGamePause);
-            GameEvents.onGameUnpause.Remove(OnGameUnPause);
-            GameEvents.VesselSituation.onReachSpace.Remove(OnEnterSpace);
+            GameEvents.onGamePause.Remove(Pause);
+            GameEvents.onGameUnpause.Remove(UnPause);
         }
-
-        private void OnEnterSpace(object o) {
-            UnPause();
-            currentTrack = int.MaxValue; // force a re-shuffle in case the player "dips" into the atmosphere
-        }
-        private void OnGamePause() => Pause();
-        private void OnGameUnPause() => UnPause();
 
         #endregion
     }
